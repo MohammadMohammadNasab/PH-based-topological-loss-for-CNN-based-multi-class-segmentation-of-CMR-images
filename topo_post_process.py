29cc7058c4e9b5ab87ebe620e5519328e3c9802f
mohammad <mmn137555@gmail.com>
2025-01-28 16:14:53 +0330

    calrifed which method is used (multi class or single class priors in saved weight)

--- topo_post_process.py
+++ topo_post_process.py
@@ -120,7 +120,7 @@ model_TP = multi_class_topological_post_processing(
 # Replace the simple model saving with more detailed version
 torch.save({
     'model_state_dict': model_TP.state_dict()
-    }, args.save_path + 'multi_class_topo.pth' if MULTI_CLASS else 'single_class_topo.pth')
+    }, args.save_path.parent / ('multi_class_topo.pth' if MULTI_CLASS else 'single_class_topo.pth'))
 
 
 # %% [markdown]
